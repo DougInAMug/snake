7065867912e310a2519dec9f7645861fe25cd484
Doug <dmhwebb@gmail.com>
2023-03-16 14:46:07 +0100

    reduce number of state variables, switch key presses to switch syntax

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,26 @@ import './style.css'
 import { ROWS, COLS } from  './gameConfig'
 import { Coordinate, DirectionKeys } from './type'
 
+
+// Colours and styles
 const appleColor = "bg-red-500";
 const snakeColor = "bg-green-500";
 const cellColor  = "bg-slate-200";
 const cellStyles = ["h-4", "w-4", "border-black", "border-1", "border-solid"];
 
+// Game initialization stuff
 const snakeCells: Array<Coordinate> = [];
 const initialSnakeCells: Array<Coordinate> = [[9, 10], [10, 10], [11, 10]];
-let   appleCell: number[] = []
-let applePresent: boolean = false;
-let direction: DirectionKeys = "R";
-let score: number = 0;
-let gameEndCondition: boolean = false;
+let   appleCell: number[] = [];
+let   direction: DirectionKeys = "ArrowRight";
+let   score: number = 0;
+let   gameEndCondition: boolean = false;
 
 const directions = { 
-    U: [ 0, -1],
-    D: [ 0,  1],
-    L: [-1,  0],
-    R: [ 1,  0]
+    "ArrowUp":    [ 0, -1],
+    "ArrowDown":  [ 0,  1],
+    "ArrowLeft":  [-1,  0],
+    "ArrowRight": [ 1,  0]
 }
 
 function modulus(n: number, m: number) {
@@ -58,27 +60,30 @@ function createGame() {
 
 function initializeSnake() {
     for (let i = 0; i < initialSnakeCells.length; i++) {
-        switchCellColorFromTo(initialSnakeCells[i], cellColor, snakeColor)
+        switchCellColorFromTo(initialSnakeCells[i], cellColor, snakeColor);
         snakeCells.push(initialSnakeCells[i]);
     }
 }
 
 function snakeMove(direction: DirectionKeys) {
-    // remove cell at end of snake
-
-    // at new cell to start of snake, based on direction
     let head = snakeCells[snakeCells.length - 1];
     let nextHead0 = modulus(head[0] + directions[direction][0], ROWS);
     let nextHead1 = modulus(head[1] + directions[direction][1], ROWS);
-    let nextHead: Coordinate = [nextHead0, nextHead1]
+    let nextHead: Coordinate = [nextHead0, nextHead1];
+
+    for (let snakeCell of snakeCells) {
+        if (snakeCell[0] == nextHead[0] && snakeCell[1] == nextHead[1]) {
+            gameEndCondition = true;
+            return;
+        }
+    }
+
     if (nextHead[0] == appleCell[0] && nextHead[1] == appleCell[1] ) {
         score++;
         const scoreDisplay = document.getElementById("playerScore")!
         scoreDisplay.innerText = `${score}`;
         switchCellColorFromTo(nextHead, snakeColor, appleColor);
-        applePresent = false;
         appleCell = [];
-        gameEndCondition = true;
     } else {
         switchCellColorFromTo(nextHead, snakeColor, cellColor);
         let tail: Coordinate = snakeCells[0];
@@ -89,26 +94,23 @@ function snakeMove(direction: DirectionKeys) {
 }
 
 function placeApple() {
-    if (applePresent === false) {
+    if (appleCell.length == 0) {
         let placementCoord: Coordinate = randomCoord();;
         while (snakeCells.includes(placementCoord)) {
             placementCoord = randomCoord();
         }
         appleCell.push(...placementCoord);
-        console.log(appleCell);
         switchCellColorFromTo(placementCoord, appleColor, cellColor);
     }
-    applePresent = true;
 }
-
 
 function gameLoop() {
     if (gameEndCondition == false) {
         setTimeout(() => {
-            window.requestAnimationFrame(gameLoop)
-            placeApple()
+            window.requestAnimationFrame(gameLoop);
+            placeApple();
             snakeMove(direction);
-        }, 1000)
+        }, 1000);
     }
 }
 
@@ -117,32 +119,33 @@ window.requestAnimationFrame(gameLoop);
 createGame();
 initializeSnake();
 
-
-let readArea = document.querySelector("html");
-readArea?.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowUp") {
-        let arrowDirection: DirectionKeys = "U";
-        if (direction !== "D") {
-            direction = arrowDirection;
-            snakeMove("U")
-        }
-    } else if (e.key === "ArrowDown") {
-        let arrowDirection: DirectionKeys = "D";
-        if (direction !== "U") {
-            direction = arrowDirection;
-            snakeMove("D")
-        }
-    } else if (e.key === "ArrowLeft") {
-        let arrowDirection: DirectionKeys = "L";
-        if (direction !== "R") {
-            direction = arrowDirection;
-            snakeMove("L")
-        }
-    } else if (e.key === "ArrowRight") {
-        let arrowDirection: DirectionKeys = "R";
-        if (direction !== "L") {
-            direction = arrowDirection;
-            snakeMove("R")
+document.querySelector("html")?.addEventListener("keydown", (e) => {
+    if (gameEndCondition === false) {
+        switch (e.key) {
+            case "ArrowUp":
+                if (direction !== "ArrowDown") {
+                    direction = "ArrowUp";
+                    snakeMove(direction);
+                }
+                break;
+            case "ArrowDown":
+                if (direction !== "ArrowUp") {
+                    direction = "ArrowDown"
+                    snakeMove(direction);
+                }
+                break;
+            case "ArrowLeft":
+                if (direction !== "ArrowRight") {
+                    direction = "ArrowLeft";
+                    snakeMove(direction);
+                }
+                break;
+            case "ArrowRight":
+                if (direction !== "ArrowLeft") {
+                    direction = "ArrowRight";
+                    snakeMove(direction);
+                }
+                break;
         }
     }
-})
+});
--- src/type.ts
+++ src/type.ts
@@ -1,2 +1,2 @@
 export type Coordinate = [number, number];
-export type DirectionKeys = "U" | "D" | "R" | "L";
+export type DirectionKeys = "ArrowUp" | "ArrowDown" | "ArrowRight" | "ArrowLeft";
